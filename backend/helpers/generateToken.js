7ecfa686e06ec5c1abc80ab3f17b2e53ccb676e2
Omelianov Nicolai <omelianov.n@gmail.com>
2023-12-13 01:46:30 +0200

    fixed creating token

--- backend/helpers/generateToken.js
+++ backend/helpers/generateToken.js
@@ -3,8 +3,14 @@ const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = process.env;
 
 const generateToken = (id) => {
-  const token = jwt.sign({ payload: { id } }, SECRET_KEY, { expiresIn: "24h" });
-  return token;
+  try {
+    const token = jwt.sign({ payload: { id } }, SECRET_KEY, { expiresIn: "24h" });
+    console.log("Generated Token:", token);
+    return token;
+  } catch (error) {
+    console.error("Token generation error:", error);
+    throw error;
+  }
 };
 
 module.exports = generateToken;
